8e19c600ef44bd96b7299fbc0bd53f29059a6525
khairallah17 <med.khairallah3@gmail.com>
2024-07-28 16:05:06 +0100

    husky setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom"
 
 const App: React.FC = () => (
     <main className="flex items-center justify-center h-screen w-screen flex-col">
+      <h1>hot reloading shiit</h1>
+      <h1>YESSSSSSS working asdfasf hot reloading</h1>
       <h1>Welcome to mah-pong</h1>
       <Link to="/login">
         Login
